ce1a1148b97378b33724483b357ec1a152d12eff
Dave Ray <daveray@gmail.com>
2011-10-12 00:02:50 -0400

    Don't event think of calling this anything but 0.0.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject congomongo-mq "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject congomongo-mq "0.0.0-SNAPSHOT"
+  :description "A message queue in mongo"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [congomongo "0.1.7"]])
