23f6a7d22df671a63747ad3ef0a76c979c9aea13
Dave Ray <daveray@gmail.com>
2011-10-13 22:39:42 -0400

    tests!

--- src/congomongo_mq/core.clj
+++ src/congomongo_mq/core.clj
@@ -19,12 +19,26 @@
     (+ (.. (java.util.Date.) getTime) offset-millis)))
 
 (defn init!
-  "Initialize a queue in collection named by queue and max size max."
+  "Initialize a queue in collection named by queue and max size max.
+  
+  Must be called inside (with-mongo).
+
+  Examples:
+  
+    (init! :my-queue 10000)
+  "
   [queue max]
   (mongo/create-collection! queue :capped true :max max))
 
 (defn enqueue! 
-  "Enqueue some content"
+  "Enqueue some content
+  
+  Must be called inside (with-mongo).
+
+  Examples:
+    
+    (enqueue! :my-queue {:x 10 :y 99})
+  "
   [queue content]
   (mongo/insert! queue {:in-prog false 
                         :done    false 
@@ -36,11 +50,33 @@
   You must call (congomongo-mq.core/done!) when you're done with the message.
   Better yet, use (congomongo-mq.core/with-next-message*).
   
-  Returns nil if no messages are available."
+  Returns nil if no messages are available.
+
+  Must be called inside (with-mongo).
+
+  Examples:
+
+    (dequeue! :my-queue)
+    ;=> nil
+    (enqueue! :my-queue {:x 10 :y 99})
+    (def m (dequeue! :my-queue))
+    ;=> A message
+    (content m)
+    ;=> {:x 10 :y 99}
+    (done! m) 
+  "
   [queue]
   (mongo/fetch-and-modify queue 
                           {:in-prog false :done false} 
                           {:$set { :in-prog true :start (timestamp 0) } } :sort {:_id 1}))
+
+(defn drain! 
+  "Dequeue messages until nil is returned and return them."
+  [queue]
+  (loop [acc [] next (dequeue! queue)]
+    (if next
+      (recur (conj acc next) (dequeue! queue))
+      acc)))
 
 (defn content 
   "Retrieve the content of a message dequeued with (congomongo-mq.core/dequeue!)."
@@ -48,7 +84,11 @@
   (:content message))
 
 (defn done!
-  "Finish processing a message retrieved with (congomongo-mq.core/dequeue!)"
+  "Finish processing a message retrieved with (congomongo-mq.core/dequeue!)
+  Consider using (congomongo-mq.core/with-next-message) instead.
+  
+  Must be called inside (with-mongo).
+  "
   [queue message]
   (mongo/update! queue 
                  {:_id (:_id message)} 
@@ -57,12 +97,40 @@
 
 (defn with-next-message* 
   "Dequeue a message, call (f (content message) & args), and call (congomongo-mq.core/done!).
-  Returns the result of f, or nil if no message was available."
+  Returns the result of f, or nil if no message was available.
+  
+  Must be called inside (with-mongo).
+
+  See:
+    (congomongo-mq.core/with-next-message)
+  "
   [queue f & args]
   (when-let [message (dequeue! queue)]
     (let [result (apply f (content message) args)]
       (done! queue message)
       result)))
+
+(defmacro with-next-message
+  "Dequeue a message, bind it to v and evalute body, returning the result
+  of the body. Calls (congomongo-mq.core/done!) automatically.
+
+  Must be called inside (with-mongo).
+  
+  Examples:
+  
+    ; Suppose next message is {:first-name \"Foo\" :last-name \"Bar\" }
+    (with-next-message {:keys [first-name last-name]}
+      (str last-name \", \" first-name))
+    ;=> \"Bar, Foo\"
+
+  See:
+    (congomongo-mq.core/with-next-message*)
+  "
+  [queue v & body]
+  `(with-next-message*
+     ~queue
+     (fn [~v]
+       ~@body)))
 
 (defn gc!
   "Run garbage collection on the queue, that is reset any messages that have taken too
--- test/congomongo_mq/test/core.clj
+++ test/congomongo_mq/test/core.clj
@@ -1,6 +1,71 @@
+;  Copyright (c) Dave Ray, 2011. All rights reserved.
+
+;   The use and distribution terms for this software are covered by the
+;   Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+;   which can be found in the file epl-v10.html at the root of this 
+;   distribution.
+;   By using this software in any fashion, you are agreeing to be bound by
+;   the terms of this license.
+;   You must not remove this notice, or any other, from this software.
+
 (ns congomongo-mq.test.core
+  (:require [somnium.congomongo :as mongo])
   (:use [congomongo-mq.core])
   (:use [clojure.test]))
 
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
+(def test-db "congomongo-mq-test")
+(def test-db-host "127.0.0.1")
+
+(defn setup! [] (mongo/mongo! :db test-db :host test-db-host))
+(defn teardown! [] (mongo/drop-database! test-db))
+
+(defmacro with-test-mongo [& body]
+  `(try
+     (setup!)
+     ~@body
+     (finally (teardown!))))
+
+(deftest message-queue 
+  (testing "init! creates a capped collection"
+    (with-test-mongo
+      (init! :test-queue 5)
+      (is (mongo/collection-exists? :test-queue))
+      (doseq [i (range 10)]
+        (enqueue! :test-queue i))
+      (is (= 5 (count (mongo/fetch :test-queue))))))
+
+  (testing "dequeue! returns nil on an empty queue"
+    (with-test-mongo
+      (init! :dequeue 5)
+      (is (nil? (dequeue! :dequeue)))))
+
+  (testing "is fifo"
+    (with-test-mongo
+      (init! :fifo 10)
+      (doseq [i (range 10)]
+        (enqueue! :fifo i))
+      (is (= (range 10) (map content (drain! :fifo)))))))
+
+(deftest garbage-collection
+  (testing "gc will return unprocessed messages to the queue"
+    (with-test-mongo
+      (init! :gc-queue 5)
+      (enqueue! :gc-queue "message")
+      (is (= "message" (content (dequeue! :gc-queue))))
+      (is (= 1 (gc! :gc-queue 0)))
+      (is (= "message" (content (dequeue! :gc-queue))))))
+  (testing "gc won't touch a done! message"
+    (with-test-mongo
+      (init! :gc-queue 10)
+      (enqueue! :gc-queue "message")
+      (done! :gc-queue (dequeue! :gc-queue))
+      (gc! :gc-queue 0)
+      (is (nil? (dequeue! :gc-queue)))))
+  (testing "with-next-message ensures that done! is called"
+    (with-test-mongo
+      (init! :gc-queue 10)
+      (enqueue! :gc-queue  "hello")
+      (is (= "HELLO" (with-next-message :gc-queue m
+        (.toUpperCase "hello"))))
+      (is (= 0 (gc! :gc-queue 0))))))
+
